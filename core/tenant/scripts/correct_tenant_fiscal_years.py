c6b63f6706d5f952c39568a0d4d860882f677b4c
Rendani Sinyage <sinyage@gmail.com>
2026-01-15 04:07:55 +0200

    Update correct_tenant_fiscal_years.py

--- core/tenant/scripts/correct_tenant_fiscal_years.py
+++ core/tenant/scripts/correct_tenant_fiscal_years.py
@@ -48,7 +48,7 @@ def run_correction():
 
                 command = [
                     "bench", "--site", site, "execute",
-                    "rokct.rokct.tenant.api.update_fiscal_year_if_default",
+                    "core.tenant.api.update_fiscal_year_if_default",
                     "--kwargs", json.dumps({"start_date": start_date})
                 ]
 
