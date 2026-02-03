473c285cf854a7628952d7bf9fe3fa0999231c86
Rendani Sinyage <sinyage@gmail.com>
2026-02-03 13:08:40 +0200

    Update test_kpi.py

--- rcore/rcore/doctype/kpi/test_kpi.py
+++ rcore/rcore/doctype/kpi/test_kpi.py
@@ -1,12 +1,52 @@
 # Copyright (c) 2025 ROKCT INTELLIGENCE (PTY) LTD
-# For license information, please see license.txt
-# Copyright (c) 2025 ROKCT INTELLIGENCE (PTY) LTD
 # See license.txt
 
 import frappe
 from frappe.tests.utils import FrappeTestCase
 
-
 class TestKPI(FrappeTestCase):
-	pass
+    def setUp(self):
+        # Create a Strategic Objective to link to
+        if not frappe.db.exists("Strategic Objective", "Test Strat Obj"):
+            self.strat_obj = frappe.get_doc({
+                "doctype": "Strategic Objective",
+                "title": "Test Strat Obj"
+            }).insert(ignore_permissions=True)
+        else:
+            self.strat_obj = frappe.get_doc("Strategic Objective", "Test Strat Obj")
 
+    def tearDown(self):
+        frappe.db.rollback()
+
+    def test_create_kpi(self):
+        kpi = frappe.get_doc({
+            "doctype": "KPI",
+            "title": "Test KPI",
+            "description": "Test Description",
+            "strategic_objective": self.strat_obj.name
+        }).insert(ignore_permissions=True)
+
+        self.assertTrue(frappe.db.exists("KPI", kpi.name))
+        self.assertEqual(kpi.strategic_objective, "Test Strat Obj")
+
+    def test_update_kpi(self):
+        kpi = frappe.get_doc({
+            "doctype": "KPI",
+            "title": "Test KPI Update",
+            "strategic_objective": self.strat_obj.name
+        }).insert(ignore_permissions=True)
+
+        kpi.description = "Updated Description"
+        kpi.save(ignore_permissions=True)
+
+        self.assertEqual(frappe.db.get_value("KPI", kpi.name, "description"), "Updated Description")
+
+    def test_delete_kpi(self):
+        kpi = frappe.get_doc({
+            "doctype": "KPI",
+            "title": "Test KPI Delete",
+            "strategic_objective": self.strat_obj.name
+        }).insert(ignore_permissions=True)
+
+        kpi.delete(ignore_permissions=True)
+        self.assertFalse(frappe.db.exists("KPI", kpi.name))
