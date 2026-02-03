4e04570057b5dc45b1dfc4879bb0f41a3ac30700
Rendani Sinyage <sinyage@gmail.com>
2026-02-03 13:08:55 +0200

    Update test_strategic_objective.py

--- rcore/rcore/doctype/strategic_objective/test_strategic_objective.py
+++ rcore/rcore/doctype/strategic_objective/test_strategic_objective.py
@@ -1,12 +1,42 @@
 # Copyright (c) 2025 ROKCT INTELLIGENCE (PTY) LTD
-# For license information, please see license.txt
-# Copyright (c) 2025 ROKCT INTELLIGENCE (PTY) LTD
 # See license.txt
 
 import frappe
 from frappe.tests.utils import FrappeTestCase
 
-
 class TestStrategicObjective(FrappeTestCase):
-	pass
+    def setUp(self):
+        # Create a Pillar to link to
+        if not frappe.db.exists("Pillar", "Test Pillar For Strat"):
+            self.pillar = frappe.get_doc({
+                "doctype": "Pillar",
+                "title": "Test Pillar For Strat",
+                "vision": "Test Vision" # Assuming Vision exists or not strictly required for this unit test if loose link
+            }).insert(ignore_permissions=True)
+        else:
+            self.pillar = frappe.get_doc("Pillar", "Test Pillar For Strat")
 
+    def tearDown(self):
+        frappe.db.rollback()
+
+    def test_create_strat_obj(self):
+        obj = frappe.get_doc({
+            "doctype": "Strategic Objective",
+            "title": "Test Strat Obj",
+            "pillar": self.pillar.name
+        }).insert(ignore_permissions=True)
+
+        self.assertTrue(frappe.db.exists("Strategic Objective", obj.name))
+        self.assertEqual(obj.pillar, self.pillar.name)
+
+    def test_update_strat_obj(self):
+        obj = frappe.get_doc({
+            "doctype": "Strategic Objective",
+            "title": "Test Obj Update",
+            "pillar": self.pillar.name
+        }).insert(ignore_permissions=True)
+
+        obj.description = "New Desc"
+        obj.save(ignore_permissions=True)
+        
+        self.assertEqual(frappe.db.get_value("Strategic Objective", obj.name, "description"), "New Desc")
